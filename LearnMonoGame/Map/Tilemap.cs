bcfffaf9d24c53038411cb768a8e0a09922c5208
HanHangit <clonkcool@yahoo.de>
2017-01-21 23:54:51 +0100

    Türme ahoi ;)

--- LearnMonoGame/Map/Tilemap.cs
+++ LearnMonoGame/Map/Tilemap.cs
@@ -10,18 +10,20 @@ namespace LearnMonoGame.Map
 {
     public class Tilemap
     {
-        Tile[,] _tileMap;
+        Tile[,,] _tileMap;
         Point _tileSize;
         int width;
         int height;
+        int layerHeight;
 
 
         public Tilemap(Texture2D[] textures, Texture2D bitMap, Point _tileSize)
         {
+            layerHeight = 5;
             this._tileSize = _tileSize;
             this.width = bitMap.Width;
             this.width = bitMap.Height;
-            this._tileMap = new Tile[bitMap.Width, bitMap.Height];
+            this._tileMap = new Tile[bitMap.Width, bitMap.Height, layerHeight];
 
             BuildMap(textures, bitMap);
         }
@@ -43,16 +45,21 @@ namespace LearnMonoGame.Map
                 {
                     if (colores[y * _tileMap.GetLength(0) + x] == Color.White) //Weiß = Gras auf der TileMap
                     {
-                        _tileMap[x, y] = new Tile(textures[0], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)),y * _tileSize.Y), ETile.Terrain);
+                        _tileMap[x, y,0] = new Tile(textures[0], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)),y * _tileSize.Y), ETile.Terrain);
                     }
                     else if (colores[y * _tileMap.GetLength(0) + x] == new Color(0, 162, 232, 255))
                     {
-                        _tileMap[x, y] = new Tile(textures[2], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y), ETile.Terrain);
+                        _tileMap[x, y,0] = new Tile(textures[2], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y), ETile.Terrain);
                     }
 
                     else //sonst Stein
                     {
-                        _tileMap[x, y] = new Tile(textures[1], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y), ETile.stone);
+                        _tileMap[x, y,0] = new Tile(textures[1], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y), ETile.stone);
+                        _tileMap[x, y, 1] = new Tile(textures[1], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y - 2*_tileSize.Y), ETile.stone);
+                        _tileMap[x, y, 2] = new Tile(textures[1], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y - 4 * _tileSize.Y), ETile.stone);
+                        _tileMap[x, y, 3] = new Tile(textures[1], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y - 6 * _tileSize.Y), ETile.stone);
+                        _tileMap[x, y, 4] = new Tile(textures[1], new Vector2(x * _tileSize.X + (_tileSize.X / 2 * (y % 2)), y * _tileSize.Y - 8 * _tileSize.Y), ETile.stone);
+
                     }
                 }
             }
@@ -77,7 +84,15 @@ namespace LearnMonoGame.Map
 
                 for (int x = 0; x < _tileMap.GetLength(0); x++)
                 {
-                    _tileMap[x, y].Draw(spritebatch);
+                    _tileMap[x, y, 0].Draw(spritebatch);
+
+
+                    if (_tileMap[x, y, 1] == null)
+                        continue;
+
+
+                    for (int z = 0; z < _tileMap.GetLength(2); z++)
+                        _tileMap[x, y, z].Draw(spritebatch);
                 }
             }
 
