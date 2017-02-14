caa138c247f839497a13ba7736f310dfdd0b0045
Razzorior <christopher94.olson@gmail.com>
2017-02-15 00:46:31 +0100

    "Bite" hinzugefuegt Überarbeitete Wolf-KI zum Testen

--- LearnMonoGame/Character/MonsterComponents/Enemies/Wolf.cs
+++ LearnMonoGame/Character/MonsterComponents/Enemies/Wolf.cs
@@ -9,13 +9,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearnMonoGame.Weapons;
 
 namespace LearnMonoGame.Summoneds.Enemies.Monster
 {
     class Wolf : Character
     {
-        private int smell_distance = 320;
-        private bool chaseMode = false;
+        protected int smell_distance = 320;
+        protected int time_till_charge_sec = 5;
+
+        protected int time_to_reach;
+        private bool chaseModeTrigger = false; // Player recognised 
+        private bool aggroTrigger = false; // Player didnt leave Wolf alone
+        private bool runTrigger = false;
+       // private bool 
 
         public Wolf(Vector2 _pos) : base(SummonedsInformation.Instance.characterInformation["Wolf"])
         {
@@ -24,6 +31,8 @@ namespace LearnMonoGame.Summoneds.Enemies.Monster
             selectedTexture = _CM.GetTexture(_CM.TextureName.damageselect);
             lifeTexture = _CM.GetTexture(_CM.TextureName.backLife);
             Initialize();
+            weapon = new Weapon(new SAbility(EMoveType.Attack, EStatus.Normal, _name: "Bite", _damage: new[] { 1, 3 }, _crit: new[] { 2f, 4f }, _critChance: 60), 0.5f, 70);
+
         }
         protected override void Initialize()
         {
@@ -36,13 +45,38 @@ namespace LearnMonoGame.Summoneds.Enemies.Monster
         }
         public override void Update(GameTime gameTime)
         {
-            chaseMode = smell();
-            if (chaseMode)
+            chaseModeTrigger = smell();
+            if (chaseModeTrigger && !aggroTrigger)
+            {
+                time_to_reach = gameTime.TotalGameTime.Seconds + time_till_charge_sec;
+                aggroTrigger = true;
+            } 
+            else if (aggroTrigger && !runTrigger)
+            {
+                if(gameTime.TotalGameTime.Seconds >= time_to_reach)
+                {
+                    runTrigger = true;
+                }
+                
+            } else if (runTrigger)
             {
                 Vector2 dif = PlayerManager.Instance.MyPlayer.Pos - pos;
                 Move(gameTime, dif);
+            } else 
+            {
+                aggroTrigger = false;
+                chaseModeTrigger = false;
             }
             base.Update(gameTime);
+        }
+
+        protected override void WeaponUpdate(GameTime gameTime, EAlignment alignment)
+        {
+
+            if (weaponStatus == EWeaponStatus.TargetFound)
+                weaponStatus = EWeaponStatus.Channel;
+
+            base.WeaponUpdate(gameTime, alignment);
         }
 
         // Riecht, ob der Player sich in der Nähe aufhält.
@@ -59,15 +93,21 @@ namespace LearnMonoGame.Summoneds.Enemies.Monster
             }
             else
             {
+                aggroTrigger = false;
+                runTrigger = false;
                 return false;
             }
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
-            if (chaseMode)
+            if (chaseModeTrigger && !runTrigger)
             {
-                Vector2 scale = new Vector2(-30,-20);
-                spriteBatch.DrawString(_CM.GetFont(_CM.FontName.Arial), "WOOOOOOOOOO", Pos + scale, Color.Red);
+                Vector2 scale = new Vector2(-25,-20);
+                spriteBatch.DrawString(_CM.GetFont(_CM.FontName.Arial), "GRRRRRRRR", Pos + scale, Color.Red);               
+            } else if (runTrigger)
+            {
+                Vector2 scale = new Vector2(-30, -20);
+                spriteBatch.DrawString(_CM.GetFont(_CM.FontName.Arial), "WOOOOOOOOO", Pos + scale, Color.Red);
             }
                 
             base.Draw(spriteBatch);
