93655b5ee3ec298f03fd77a62a5008e27bbba164
HanHangit <hauffej@yahoo.de>
2017-02-27 22:06:11 +0100

    DebugConsole ftw

--- LearnMonoGame/Program.cs
+++ LearnMonoGame/Program.cs
@@ -12,7 +12,7 @@ namespace LearnMonoGame
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             using (var game = new Game1())
                 game.Run();
